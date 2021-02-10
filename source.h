3bf66fa351a9fb79a5c58f6eeeaeeebd19776422
TrungDinhT <dinhthanhtrung1996@gmail.com>
2021-02-10 22:24:59 +0100

    first commit

---
+++ source.h
@@ -0,0 +1,24 @@
+#include <vector>
+#include <string>
+
+using namespace std;
+
+struct Pizza 
+{
+    int id;
+    vector<string>> ingredients;
+};
+
+struct Team
+{
+    int numberTeam2;
+    int numberTeam3;
+    int numberTeam4;
+};
+
+struct Delivery
+{
+    int teamId;
+    vector<int> pizzaIds;
+};
+
